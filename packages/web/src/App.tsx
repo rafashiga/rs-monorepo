2c14521e44c575f436bb1836bd38dd134311b388
Rafael <rafa.shiga@gmail.com>
2020-07-26 17:36:29 -0300

    feat(axios): add config

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react'
-// import api from '@omnirepo/axios-config'
+import api from '@monorepo/axios-config'
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
-  // useEffect(() => {
-  // 	api.get('/').then((response) => {
-  // 		console.log(response);
-  // 	});
-  // }, []);
+  useEffect(() => {
+    api.get('/').then(response => {
+      console.log(response)
+    })
+  }, [])
 
   return <h1>Hello World</h1>
 }
